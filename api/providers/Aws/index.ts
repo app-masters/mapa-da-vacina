bfd45618e9d086a19fdd5b9148693f2bd7996c3f
Bruno Pinheiro <brunomarcospsilva@gmail.com>
2021-04-08 16:46:16 -0300

    Change error return

--- api/providers/Aws/index.ts
+++ api/providers/Aws/index.ts
@@ -6,6 +6,8 @@ export default class Aws {
   private _sns: SNSClient;
   private _devPhone: string;
   private _nodeEnv: string;
+  private _lastError: Error;
+
   /**
    * Constructor
    * @param Config
@@ -27,35 +29,49 @@ export default class Aws {
    * @param params SMS data parameters
    */
   public async dispatchSMS(toNumber: string, content: string) {
+    // When not in production, all the phone numbers should be replaced to the DEV_PHONE address in the .ENV file
+    let phoneNumber = toNumber;
+    if (this._nodeEnv === 'development') {
+      if (!this._devPhone) {
+        this._lastError = new Error('Missing AWS_DEV_PHONE variable in .env file');
+        return false;
+      }
+      phoneNumber = this._devPhone;
+    }
+
+    // Validates the phone number
+    if (!validatePhone(phoneNumber)) {
+      this._lastError = new Error(`Invalid recipient phone number: ${phoneNumber}`);
+      return false;
+    }
+
+    // Check if the content size is less than 140bytes long
+    // see: https://docs.aws.amazon.com/sns/latest/dg/sms_publish-to-phone.html
+    if (Buffer.byteLength(content) > 140) {
+      this._lastError = new Error(`Message content exceeds the maximum of 140 bytes`);
+      return false;
+    }
+
+    const SNSParams: PublishCommandInput = {
+      PhoneNumber: formatPhoneToE164(phoneNumber),
+      Message: content
+    };
+
     try {
-      // When not in production, all the phone numbers should be replaced to the DEV_PHONE address in the .ENV file
-      let phoneNumber = toNumber;
-      if (this._nodeEnv === 'development') {
-        if (!this._devPhone) throw new Error('Missing AWS_DEV_PHONE variable in .env file');
-        phoneNumber = this._devPhone;
-      }
-
-      // Validates the phone number
-      if (!validatePhone(phoneNumber)) {
-        throw new Error(`Invalid recepient phone number: ${phoneNumber}`);
-      }
-
-      // Check if the content size is less than 140bytes long
-      // see: https://docs.aws.amazon.com/sns/latest/dg/sms_publish-to-phone.html
-      if (Buffer.byteLength(content) > 140) {
-        throw new Error(`Message content exceeds the maximum of 140 bytes`);
-      }
-
-      const SNSParams: PublishCommandInput = {
-        PhoneNumber: formatPhoneToE164(phoneNumber),
-        Message: content
-      };
-
-      const data = await this._sns.send(new PublishCommand(SNSParams));
-      console.log('Real SMS sent - to: ' + SNSParams.PhoneNumber + ' - message: ' + SNSParams.Message);
-      return data;
+      console.log('Sending real SMS - to: ' + SNSParams.PhoneNumber + ' - message: ' + SNSParams.Message);
+      await this._sns.send(new PublishCommand(SNSParams));
+      return true;
     } catch (err) {
       console.error(err, err.stack);
+      this._lastError = err;
+      return false;
     }
+  }
+
+  /**
+   * Return last occurred error
+   */
+  public lastError() {
+    return this._lastError;
   }
 }
