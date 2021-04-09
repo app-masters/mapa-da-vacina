736bc2b3277c69bbb5968135e6940403e3e0e33b
Bruno Pinheiro <brunomarcospsilva@gmail.com>
2021-04-08 22:03:53 -0300

    Bootstratp validator

---
+++ api/app/Controllers/Http/UsersController.ts
@@ -0,0 +1,11 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import UserValidator from 'App/Validators/UserValidator'
+
+export default class UsersController {
+  /**
+   * Invite a new user
+   */
+  public async invite({ request, response }: HttpContextContract) {
+
+  }
+}
---
+++ api/start/validationRules.ts
@@ -0,0 +1,2 @@
+import { validator } from '@ioc:Adonis/Core/Validator'
+import { parsePhoneNumberFromString } from 'libphonenumber-js'
