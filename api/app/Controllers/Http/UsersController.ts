2c2ef89baae6b8762e889007a1bb85bf714b44e3
Bruno Pinheiro <brunomarcospsilva@gmail.com>
2021-04-09 16:48:17 -0300

    Add validate user route

--- api/app/Controllers/Http/UsersController.ts
+++ api/app/Controllers/Http/UsersController.ts
@@ -1,9 +1,12 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 import UserValidator from 'App/Validators/UserValidator';
-import UserRepository from 'App/Models/User';
+import ValidateValidator from 'App/Validators/ValidateValidator';
+import UserRepository, { UserType } from 'App/Models/User';
 import SmsMessages from 'App/Models/Messages/SmsMessages';
 import Prefecture from 'App/Models/Prefecture';
-import Place, { PlaceType } from 'App/Models/Place';
+import Place from 'App/Models/Place';
+import FirebaseProvider from '@ioc:Adonis/Providers/Firebase';
+import Admin from 'App/Models/Admin';
 export default class UsersController {
   /**
    * Invite a new user
@@ -36,6 +39,50 @@ export default class UsersController {
       await SmsMessages.sendInviteSms(newUser, prefecture.name, placeTitle);
 
       return response.status(200).send(newUser);
+    } catch (error) {
+      return response.status(500).send(error);
+    }
+  }
+
+  /**
+   * Validate a new user
+   */
+  public async validate({ request, response }: HttpContextContract) {
+    try {
+      const data = await request.validate(ValidateValidator);
+
+      // checks for our user in Firestore matching any user with the phone
+      const userSnapshot = await FirebaseProvider.db
+        .collectionGroup('user')
+        .where('phone', '==', data.phone)
+        .limit(1)
+        .get();
+
+      if (userSnapshot.docs.length > 0) {
+        const user = userSnapshot.docs[0].data() as UserType;
+        console.log('user', user);
+        // first sign in
+        if (!user.uid && !user.signedUpAt) {
+          user.uid = data.uid;
+          user.signedUpAt = new Date();
+          user.active = true;
+          await UserRepository.save(user, user.prefectureId);
+        }
+        const prefecture = Prefecture.findById(user.prefectureId);
+        const place = user.placeId ? Prefecture.findById(user.placeId) : undefined;
+        return response.status(200).send({ user, prefecture, place });
+      }
+
+      // if it wasn't a user, try and admin
+      const admin = await Admin.get({ phone: data.phone });
+      if (admin) return response.status(200).send(admin);
+
+      // if coudn't find any user
+      return response
+        .status(401)
+        .send(
+          'Seu telefone não está na lista de convites para utilizar o filômetro, fale com o responsável pelo ponto de vacinação ou da prefeitura'
+        );
     } catch (error) {
       return response.status(500).send(error);
     }
--- api/app/Middleware/Auth.ts
+++ api/app/Middleware/Auth.ts
@@ -1,6 +1,6 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 import FirebaseProvider from '@ioc:Adonis/Providers/Firebase';
-import UserRepository from 'App/Models/User';
+import { UserType } from 'App/Models/User';
 import AdminRepository from 'App/Models/Admin';
 
 export default class Auth {
@@ -20,20 +20,21 @@ export default class Auth {
       console.log('decodedToken', JSON.stringify(decodedToken));
       const phone = decodedToken.phone_number;
 
-      // then checks for our user in Firestore
-      const prefectureId = request.post().prefectureId;
-      if (prefectureId) {
-        const user = await UserRepository.find({ phone: phone }, prefectureId);
-        if (user) {
-          request.user = user;
-        } else {
-          const userAdmin = await AdminRepository.find({ phone: phone });
-          if (userAdmin) {
-            request.user = userAdmin;
-          }
+      // then checks for our user in Firestore matching any user with the phone
+      const userSnapshot = await FirebaseProvider.db.collectionGroup('user').where('phone', '==', phone).limit(1).get();
+      if (userSnapshot.docs.length > 0) {
+        const user = userSnapshot.docs[0].data() as UserType;
+        request.user = user;
+      } else {
+        const userAdmin = await AdminRepository.find({ phone: phone });
+        if (userAdmin) {
+          request.user = userAdmin;
         }
       }
 
+      if (!request.user) {
+        return response.status(401).send({ message: 'Usuário não encontrado.' });
+      }
       // code for middleware goes here. ABOVE THE NEXT CALL
       await next();
     } catch (err) {
