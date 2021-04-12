7a8cdc089035e7c60c126e3c2ee093ba91420537
Bruno Pinheiro <brunomarcospsilva@gmail.com>
2021-04-12 16:54:56 -0300

    Remove comments

--- api/app/Middleware/Auth.ts
+++ api/app/Middleware/Auth.ts
@@ -1,7 +1,5 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 import FirebaseProvider from '@ioc:Adonis/Providers/Firebase';
-import { UserType } from 'App/Models/User';
-import AdminRepository from 'App/Models/Admin';
 
 export default class Auth {
   /**
@@ -24,22 +22,6 @@ export default class Auth {
       console.log('decodedToken', JSON.stringify(decodedToken));
       request.decodedIdToken = decodedToken;
 
-      /*const phone = decodedToken.phone_number;
-      // then checks for our user in Firestore matching any user with the phone
-      const userSnapshot = await FirebaseProvider.db.collectionGroup('user').where('phone', '==', phone).limit(1).get();
-      if (userSnapshot.docs.length > 0) {
-        const user = userSnapshot.docs[0].data() as UserType;
-        request.user = user;
-      } else {
-        const userAdmin = await AdminRepository.find({ phone: phone });
-        if (userAdmin) {
-          request.user = userAdmin;
-        }
-      }
-
-      if (!request.user) {
-        return response.status(401).send({ message: 'Usuário não encontrado.' });
-      }*/
       // code for middleware goes here. ABOVE THE NEXT CALL
       await next();
     } catch (err) {
