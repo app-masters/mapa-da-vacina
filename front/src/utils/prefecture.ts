2451273d14edb5c3e94c384c5573fd8f32592593
Baraky <jfbaraky@gmail.com>
2021-04-13 16:00:20 -0300

    Adding logs

--- front/src/utils/prefecture.ts
+++ front/src/utils/prefecture.ts
@@ -1,4 +1,5 @@
 import { Prefectures } from '../lib/Prefectures';
+// import logging from './logging';
 
 /**
  * Get prefecture ID
@@ -9,7 +10,10 @@ export const getPrefectureData = async (): Promise<Prefectures> => {
     let prefectureId = process.env.NEXT_PUBLIC_PREFECTURE_ID;
     if (!prefectureId) {
       // No prefecture id defined, getting from online variable
+      console.log('NEXT_PUBLIC_HEROKU', process.env.NEXT_PUBLIC_HEROKU);
+      console.log('window.location', window.location);
       const possibleId = window.location.host.split('.')[0];
+      console.log('subdomain', possibleId);
       if (JSON.parse(process.env.NEXT_PUBLIC_HEROKU).indexOf(possibleId) > -1) {
         prefectureId = possibleId;
       }
@@ -18,7 +22,8 @@ export const getPrefectureData = async (): Promise<Prefectures> => {
         throw Error('You need to define a NEXT_PUBLIC_PREFECTURE_ID on your .env to be able to fetch the data');
       }
     }
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/prefectures/${process.env.NEXT_PUBLIC_PREFECTURE_ID}`);
+    console.log('prefectureId', prefectureId);
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/prefectures/${prefectureId}`);
     const data = await res.json();
     return data;
   } catch (error) {
