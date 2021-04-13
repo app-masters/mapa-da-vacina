5bdef8fb4afee44e78d57f48369f574f1c757fe0
Bruno Pinheiro <brunomarcospsilva@gmail.com>
2021-04-13 09:16:52 -0300

    Fix eslint error

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,7 +1,7 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 admin.initializeApp();
-const db = admin.firestore();
+// const db = admin.firestore();
 
 export const createQueueUpdate = functions.firestore
   .document(
