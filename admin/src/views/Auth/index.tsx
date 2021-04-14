fc1c8ec348aefa88d041a50e54781f351403e983
Alessandro Macanha <alessandromacanha@gmail.com>
2021-04-14 11:02:54 -0300

    added pt

--- admin/src/views/Auth/index.tsx
+++ admin/src/views/Auth/index.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { AuthContent, AuthWrapper } from './styles';
 import Image from 'next/image';
-import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import firebase from 'firebase/app';
-import 'firebase/auth';
 
 const firebaseAuthConfig = {
   signInFlow: 'popup',
@@ -34,18 +32,22 @@ const firebaseAuthConfig = {
  * Auth view
  */
 const Auth: React.FC = () => {
-  const [renderAuth, setRenderAuth] = React.useState<boolean>(false);
   // Only renders the firebase on client side
   React.useEffect(() => {
-    if (typeof window !== 'undefined') {
-      setRenderAuth(true);
+    if (process.browser && typeof window !== 'undefined') {
+      require('firebaseui/dist/firebaseui.css');
+      const firebaseui = require('../../utils/firebaseui_pt_br');
+      const element = document.getElementById('#firebase-auth');
+      const ui = firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(firebase.auth());
+      ui.start(element, firebaseAuthConfig);
     }
   }, []);
   return (
     <AuthWrapper>
       <AuthContent>
         <Image src={'/images/logo-app.png'} width={230} height={80} />
-        {renderAuth ? <StyledFirebaseAuth uiConfig={firebaseAuthConfig} firebaseAuth={firebase.auth()} /> : null}
+        <div id="#firebase-auth" />
+        {/* {renderAuth ? <StyledFirebaseAuth uiConfig={firebaseAuthConfig} firebaseAuth={firebase.auth()} /> : null} */}
       </AuthContent>
     </AuthWrapper>
   );
