678817257f6e14f2144b7ae44c117939c236c414
Bruno Pinheiro <brunomarcospsilva@gmail.com>
2021-04-15 17:05:45 -0300

    Add tests

--- firebase/functions/tests/simple.spec.ts
+++ firebase/functions/tests/simple.spec.ts
@@ -3,6 +3,24 @@ import * as firebase from "@firebase/testing";
 
 const PROJECT_ID = "filometro-dev-3d";
 
+const authSuperAdmin = {
+  uid: "super_uid",
+  role: "superAdmin",
+};
+const authPrefectureAdmin = {
+  uid: "super_uid",
+  role: "superAdmin",
+  prefectureId: "test_pref",
+};
+const authPlaceAdmin = {
+  uid: "super_uid",
+  role: "superAdmin",
+};
+const authQueueObserver = {
+  uid: "super_uid",
+  role: "superAdmin",
+};
+
 const getFirestore = (auth: Record<string, unknown>) => {
   return firebase
     .initializeTestApp({ projectId: PROJECT_ID, auth: auth })
@@ -14,7 +32,26 @@ describe("Mapa da Vacina", () => {
     assert.strictEqual(2 + 2, 4);
   });
 
-  it("Only admin can write to a prefecture", async () => {
+  it("Super admin can write to everything", async () => {
+    const db = getFirestore(authSuperAdmin);
+    const pref = db.collection("prefecture").doc("test_pref");
+    const place = db
+      .collection("prefecture")
+      .doc("test_pref")
+      .collection("place")
+      .doc("test_place");
+    const user = db
+      .collection("prefecture")
+      .doc("test_pref")
+      .collection("user")
+      .doc("test_user");
+
+    await firebase.assertSucceeds(pref.set({ name: "Test" }));
+    await firebase.assertSucceeds(place.set({ name: "Test" }));
+    await firebase.assertSucceeds(user.set({ name: "Test" }));
+  });
+
+  it("Only Prefecture Admin can write to a prefecture", async () => {
     const myAuth = {
       uid: "test_uid",
       role: "prefectureAdmin",
