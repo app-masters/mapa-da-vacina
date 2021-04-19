70fce334b2e1bdec528230dd4dc7c290b8a0a524
Bruno Pinheiro <brunomarcospsilva@gmail.com>
2021-04-19 08:46:15 -0300

    Some adjustments

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -7,6 +7,63 @@ const client = new Client({});
 admin.initializeApp();
 const db = admin.firestore();
 
+/**
+ * Sanitize a zip, return it with only numbers, or null
+ */
+function sanitizeZip(zip: string): string {
+  if (!zip) return "";
+  const match = zip.match(/\d+/g);
+  if (!match) return "";
+  const zipCode = match.join("");
+  if (zipCode.length !== 8) return "";
+  return zipCode;
+}
+
+async function returnCoordinates(zip: string) {
+  // Checks database first
+  const dbResult = await db.collection("zipCoordinate").doc(zip).get();
+  if (dbResult.exists) return dbResult.data();
+
+  //If non-existent, check google API
+  const googleApiKey = functions.config().googlemaps.key;
+  // console.log(googleApiKey);
+  if (!googleApiKey) {
+    console.log("    👉  No GOOGLE_API_KEY, will not fetch coordinates");
+    return undefined;
+    //throw new Error("No GOOGLE_API_KEY credentials.");
+  }
+
+  const queryData = await client.geocode({
+    params: {
+      components: "postal_code:" + zip + "|country:BR",
+      key: googleApiKey,
+    },
+  });
+  console.log(queryData);
+
+  if (queryData && queryData.statusText === "OK") {
+    const geometry = queryData.data.results[0].geometry;
+
+    console.log("geometry", geometry);
+
+    const returnData = {
+      zip: zip,
+      latitude: geometry.location.lat,
+      longitude: geometry.location.lng,
+    };
+    await dbResult.ref.create(returnData);
+
+    return returnData;
+  } else if (queryData.data) {
+    // Show some log if we don't know how to handle this result
+    if (!["ZERO_RESULTS"].includes(queryData.statusText)) {
+      console.log("  ‼️ unexpected result from Google Maps", queryData);
+    }
+  }
+  return undefined;
+  //throw new Error("No results found.");
+}
+
 export const createQueueUpdate = functions.firestore
   .document(
     "prefecture/{prefectureId}/place/{placeId}/queueUpdate/{queueUpdateId}"
@@ -42,15 +99,17 @@ export const totalizeOpenPlacesCount = functions.firestore
     ) {
       returnCoordinates(afterValue?.zip)
         .then((data) => {
-          db.collection("prefecture")
-            .doc(context.params.prefectureId)
-            .collection("place")
-            .doc(context.params.placeId)
-            .update({
-              latitude: data?.latitude,
-              longitude: data?.longitude,
-              zip: data?.zip,
-            });
+          if (data) {
+            db.collection("prefecture")
+              .doc(context.params.prefectureId)
+              .collection("place")
+              .doc(context.params.placeId)
+              .update({
+                latitude: data.latitude,
+                longitude: data.longitude,
+                zip: data.zip,
+              });
+          }
         })
         .catch((err) => {
           console.log("Error setting coordinates ", err);
@@ -90,61 +149,6 @@ export const totalizeOpenPlacesCount = functions.firestore
       return null;
     }
   });
-
-/**
- * Sanitize a zip, return it with only numbers, or null
- */
-function sanitizeZip(zip: string): string {
-  if (!zip) return "";
-  const match = zip.match(/\d+/g);
-  if (!match) return "";
-  const zipCode = match.join("");
-  if (zipCode.length !== 8) return "";
-  return zipCode;
-}
-
-async function returnCoordinates(zip: string) {
-  // Checks database first
-  const dbResult = await db.collection("zipCoordinate").doc(zip).get();
-  if (dbResult.exists) return dbResult.data();
-
-  //If non-existent, check google API
-  const googleApiKey = functions.config().googlemaps.key;
-  // console.log(googleApiKey);
-  if (!googleApiKey) {
-    console.log("    👉  No GOOGLE_API_KEY, will not fetch coordinates");
-    throw new Error("No GOOGLE_API_KEY credentials.");
-  }
-
-  const queryData = await client.geocode({
-    params: {
-      components: "postal_code:" + zip + "|country:BR",
-      key: googleApiKey,
-    },
-  });
-  console.log(queryData);
-
-  if (queryData && queryData.statusText === "OK") {
-    const geometry = queryData.data.results[0].geometry;
-
-    console.log("geometry", geometry);
-
-    const returnData = {
-      zip: zip,
-      latitude: geometry.location.lat,
-      longitude: geometry.location.lng,
-    };
-    await dbResult.ref.create(returnData);
-
-    return returnData;
-  } else if (queryData.data) {
-    // Show some log if we don't know how to handle this result
-    if (!["ZERO_RESULTS"].includes(queryData.statusText)) {
-      console.log("  ‼️ unexpected result from Google Maps", queryData);
-    }
-  }
-  throw new Error("No results found.");
-}
 
 export const getCoordinates = functions.https.onRequest(async (req, res) => {
   const { data } = req.body;
